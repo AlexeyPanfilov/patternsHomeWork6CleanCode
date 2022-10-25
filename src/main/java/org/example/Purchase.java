8598b7c6a50f30688f11503a66c2901353988f6b
Alexey Panfilov <ap83@list.ru>
2022-10-25 15:07:14 +0300

    Initial commit

---
+++ src/main/java/org/example/Purchase.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Map;
+
+public class Purchase {
+
+    protected String title;
+    protected int count;
+    protected Purchase[] purchases;
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public Purchase(Map<String, Integer> prices) {
+        purchases = new Purchase[prices.size()];
+    }
+
+    public void addPurchase(String title, int count) {
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] == null) {
+                purchases[i] = new Purchase(title, count);
+                return;
+            }
+            if (purchases[i].title.equals(title)) {
+                purchases[i].count += count;
+                return;
+            }
+        }
+    }
+
+    public long sum() {
+        long sum = 0;
+        System.out.println("КОРЗИНА:");
+        for (int i = 0; i < purchases.length; i++) {
+            Purchase purchase = purchases[i];
+            if (purchase == null) continue;
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " +
+                    (purchase.count * purchases[i].count) + " руб.");
+            sum += purchase.count * purchases[i].count;
+        }
+        return sum;
+    }
+}
